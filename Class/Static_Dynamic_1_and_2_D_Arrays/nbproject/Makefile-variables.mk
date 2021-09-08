2cdfa0f44119a93ac0fe872066056fe7788a5c8b
Mark Lehr <mark.lehr@rcc.edu>
2021-09-08 08:38:47 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/Static_Dynamic_1_and_2_D_Arrays/nbproject/Makefile-variables.mk
+++ Class/Static_Dynamic_1_and_2_D_Arrays/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Debug=static_dynamic_1_and_2_d_arrays
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/static_dynamic_1_and_2_d_arrays
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/static_dynamic_1_and_2_d_arrays
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Debug=staticdynamic1and2darrays.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/staticdynamic1and2darrays.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/staticdynamic1and2darrays.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Release=static_dynamic_1_and_2_d_arrays
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/static_dynamic_1_and_2_d_arrays
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/static_dynamic_1_and_2_d_arrays
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Release=staticdynamic1and2darrays.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/staticdynamic1and2darrays.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/staticdynamic1and2darrays.tar
 #
 # include compiler specific variables
 #
