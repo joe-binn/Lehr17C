09c3203885d2115f91766fa358bf8f6cc5a6d260
Mark Lehr <mark.lehr@rcc.edu>
2021-09-09 13:17:51 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/MarkSort_opAnalysis/nbproject/Makefile-Release.mk
+++ Class/MarkSort_opAnalysis/nbproject/Makefile-Release.mk
@@ -56,11 +56,11 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/review1_cis17c_objectarray.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/marksort_opanalysis.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/review1_cis17c_objectarray.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/marksort_opanalysis.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/review1_cis17c_objectarray ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/marksort_opanalysis ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
